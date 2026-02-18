f6ca23c4fb688c589a41937eed2880b7405b64f4
Shree Elamathi <shreeelamathi@gmail.com>
2026-02-19 05:24:49 +0530

    JDBC connection established

---
+++ src/main/java/Demo.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+import java.util.*;
+
+//To have dynamic values
+public class Demo {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/school";
+        String user = "root";
+        String pass = "elamathi123";
+        Connection conn = DriverManager.getConnection(url,user,pass);
+
+        int id = 207;
+        String name = "Nimisha";
+        int marks = 460;
+
+        String query = "insert into student values(?,?,?)";
+
+        PreparedStatement st = conn.prepareStatement(query);
+        st.setInt(1,id);
+        st.setString(2,name);
+        st.setInt(3,marks);
+
+        System.out.println(st.execute());
+
+    }
+}
--- src/main/java/DemoJdbc.java
+++ src/main/java/DemoJdbc.java
@@ -1,23 +1,44 @@
 import java.sql.*;
 
-public class DemoJdbc {
 
+// To do statis changes
+//contains query and pre defined values in query
+public class DemoJdbc {
     public static void main(String[] args) throws SQLException {
         String url ="jdbc:mysql://localhost:3306/school";
         String username = "root";
         String pass = "elamathi123";
 
-        String query = "select * from student";
 
         Connection conn = DriverManager.getConnection(url,username,pass);
         System.out.println("Connection established");
         Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery(query);
-        while (rs.next()){
-            System.out.println(rs.getInt("stud_ID") + " "+rs.getString("stud_name"));
-            
-        }
-        System.out.println(rs.next());
+
+//      To fetch data
+//        String query = "select * from student";
+//        ResultSet rs = st.executeQuery(query);
+//        while (rs.next()){
+//            System.out.println(rs.getInt("stud_ID") + " "+rs.getString("stud_name"));
+//
+//        }
+//        System.out.println(rs.next());
+
+
+//        To insert data
+//        String query = "insert into student values(207,'Nimisha',460)";
+//        boolean status = st.execute(query); this gives output based on the query
+//        System.out.println(status);
+
+
+//        To update data
+//        String query = "update student set stud_name = 'Aishu' where stud_ID = 200";
+//        int status = st.executeUpdate(query);
+//        System.out.println(status);
+
+//        To delete
+//        String query = "delete from student where stud_ID = 207";
+//        st.execute(query);
+
 
         conn.close();
         System.out.println("Connection closed");
