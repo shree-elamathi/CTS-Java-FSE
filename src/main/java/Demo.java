9dda0bf0136a738a0675c9444404190b8026819a
Shree Elamathi <shreeelamathi@gmail.com>
2026-02-19 05:30:42 +0530

    Prepared Statement learnt

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,6 +2,7 @@ import java.sql.*;
 import java.util.*;
 
 //To have dynamic values
+//Have used Prepared Statement
 public class Demo {
     public static void main(String[] args) throws SQLException {
 
